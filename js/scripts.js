6f283fca0ace83f76abc774a89fe467a8809787c
Bryce Frazier and Sara Hamilton <bryce@>
2018-01-17 16:35:01 -0800

    modify function to take in variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var factorial = function(number) {
   var result = 1;
-  for (var index = 5; index > 0; index -= 1){
+  for (var index = number; index > 0; index -= 1){
     result *= index;
   }
   return result;
@@ -9,7 +9,7 @@ var factorial = function(number) {
 $(document).ready(function(){
   $("form#factorial-form").submit(function(event){
     event.preventDefault();
-    var input = $("input#input1").val();
-    $(".result").append(factorial(input));
+    var number = $("input#input1").val();
+    $(".result").append(factorial(number));
   });
 });
